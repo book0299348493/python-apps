a690a5a0619fc72fafd6ccbabe917fdac13d102c
book0299348493 <yuujirou.moto0213@gmail.com>
2025-06-12 12:11:15 +0900

    work2の基本の動作を実装

---
+++ work_01.py
@@ -0,0 +1,26 @@
+import random
+
+random_number = random.randint(1, 100)
+b = 4
+for i in range(5):
+    cnt = cnt + 1
+
+
+    if random_number == int(a):
+        print("正解です")
+        result = True
+        break
+    else:
+    print("不正解です")
+    if random_number < 
+
+
+
+
+
+if result:
+    print    
+
+
+
+
